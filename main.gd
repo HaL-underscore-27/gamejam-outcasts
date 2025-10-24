ab233f16d86670a78e6664faf65e7909e89b819d
anisa-i <anisa.isallari@student.ucll.be>
2025-10-24 20:19:46 +0200

    first commit

--- main.gd
+++ main.gd
@@ -1,1 +1,20 @@
 extends Node3D
+
+@onready var timer = $CountdownTimer
+@onready var label = $Label3D
+
+var countdown = 10  # seconds
+
+func _ready():
+	countdown = int(timer.wait_time)
+	label.text = "Time left: %d" % countdown
+	timer.start()
+	set_process(true)
+
+func _process(delta):
+	if timer.time_left > 0:
+		countdown = int(timer.time_left)
+		label.text = "Time left: %d" % countdown
+	else:
+		label.text = "Countdown finished!"
+		set_process(false)
